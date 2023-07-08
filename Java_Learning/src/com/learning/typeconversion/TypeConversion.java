8cb1efc6d1f174686931315d1a990b6edbe5c919
Mikheil <mkvashali@emis.ge>
2023-07-08 14:01:51 +0400

    Type conversions in Java

---
+++ Java_Learning/src/com/learning/typeconversion/TypeConversion.java
@@ -0,0 +1,23 @@
+package com.learning.typeconversion;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+        float floatVal = 1.0f;
+        double doubleVal = 4.0d;
+        byte byteVal = 7;
+        short shortVal = 7;
+        long longVal = 5;
+
+        short result1 = (short) longVal;
+        short result2 = (short) (byteVal - longVal);
+        float result3 = longVal - floatVal;
+
+        var v1 = 50;
+        v1 = (int) 100.0;
+
+        var thisValue = 7.5f; // thisValue is a float
+        v1 = (int) thisValue;
+
+        System.out.println("Success");
+    }
+}
